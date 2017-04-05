69870493e1869eac01b1e4529e6878c52151c932
hidta <tahkyu3@outlook.jp>
2017-04-05 22:23:25 +0900

    create 500 error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
 
   layout :set_layout
 
+  rescue_form Exception, with: :rescue500
+
   private
   
   def set_layout
@@ -15,8 +17,15 @@ class ApplicationController < ActionController::Base
     end
   end
   #set_layoutは staff, admin, customer のどれかを返し、それがレイアウトの名前として使用される
+  #レイアウト決定の優先度#レイアウト決定の優先度
+  #コントローラ名と同名のレイアウトが選択、なければapplicationレイアウトが選択される
+
+  def rescue500(e)
+    @exception = e
+    render 'errors/internal_server_error', status: 500
+  end
+  #rescueはクラス・メソッド
+  #例外が発生したらrescue500に処理を渡す
+  #renderでerrors/internal_server_error.html.erbを生成して表示
 
 end
-
-#レイアウト決定の優先度
-#コントローラ名と同名のレイアウトが選択、なければapplicationレイアウトが選択される
