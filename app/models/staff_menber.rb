2512bb4c758d0d2283d8a31c2e92eadb8a24a21b
hidta <tahkyu3@outlook.jp>
2017-04-06 22:01:49 +0900

    user hash function to chage password

--- app/models/staff_menber.rb
+++ app/models/staff_menber.rb
@@ -3,4 +3,12 @@ class StaffMenber < ActiveRecord::Base
     self.email_for_index = email.downcase if email
   end
 
+  def password=(row_password)
+    if row_password.kind_of?(String)
+      self.hashed_password = BCrypt::Password.create(row_password)
+    elsif row_password.nil?
+      self.hashed_password = nil
+    end
+  end
+
 end
