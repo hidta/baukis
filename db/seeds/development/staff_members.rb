396f2c3c1dda8eac19601e82ab3c3172c7731f51
hidta <tahkyu3@outlook.jp>
2017-04-07 15:52:19 +0900

    fixed typo

--- db/seeds/development/staff_members.rb
+++ db/seeds/development/staff_members.rb
@@ -1,9 +1,9 @@
 StaffMember.create!(
   email:           Faker::Internet.email, 
-  familiy_name:    "田中",
+  family_name:    "田中",
   given_name:      "太郎",
-  familiy_name_kana: "ヤマダ",
+  family_name_kana: "ヤマダ",
   given_name_kana:   "タロウ",
   password:         "password",
-  start_date:       "Date.today"
+  start_date:       Date.today
 )
