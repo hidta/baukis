644f249fc58afc95c333d21f1f8b2f2829dba042
hidta <tahkyu3@outlook.jp>
2017-04-07 20:31:07 +0900

    add logout controller

--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -8,4 +8,26 @@ class Staff::SessionsController < Staff::Base
     end
   end
 
+  def create
+    @form = Staff::LoginForm.new(params[:staff_login_form])
+    # Staff::LoginFormは非ActiveRecordモデルで、インスタンス生成
+    if @form.email.present?
+      staff_member = StaffMember.find_by(email_for_index: @form.email.downcase)
+    end
+    if staff_member
+      session[:staff_member_id] = staff_member.id
+      # sessionオブジェクトにstaffmemberのid属性の値をセット
+      # sessionオブジェクトはリダレクト先でも参照できる
+      # ;staff_member_idキーに値がセットされていることがログインしている状態を表す
+      redirect_to :staff_root
+    else
+      render action: 'new'
+    end
+  end
+
+  def destroy
+    session.delete(:staff_member_id)
+    redirect_to :staff_root
+  end
+
 end
